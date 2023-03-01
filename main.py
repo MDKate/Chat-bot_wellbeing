262e0b1c80991c59bab20e2b3140e7ec403d0880
DESKTOP-MHC35ME\50AdmNsk <medngueu@gmail.com>
2023-03-01 12:36:05 +0700

    Относительный путь

--- main.py
+++ main.py
@@ -8,11 +8,12 @@ import emoji
 from pathlib import Path
 from telegram import ParseMode
 import datetime
+import os
 
 from emoji import emojize
 
 # Подключаемся к боту
-bot = telebot.TeleBot('1022369852:AAFgpjNBwQYMc0Jxl2bdyeQjLx3K918jN0A')
+bot = telebot.TeleBot('')
 
 
 page = 1
@@ -38,20 +39,20 @@ def callback_query(call):
     global logs1
 
     try:
-        logs1 = pd.read_csv('C:/Users/50AdmNsk/PycharmProjects/pythonProject/Statistic.csv')
+        logs1 = pd.read_csv(os.path.abspath('Statistic.csv'))
         del (logs1['Unnamed: 0'])
         time1 = str(datetime.datetime.now().time())
         dat1 = str(datetime.datetime.now().date())
         logs1 = logs1.concat({'date': dat1, 'time': time1, 'person_id': call.message.chat.id, 'branch_id': req[0]},
                              ignore_index=True)
-        path2 = Path("C:/Users/50AdmNsk/PycharmProjects/pythonProject/Statistic.csv")
+        path2 = os.path.abspath("Statistic.csv")
         logs1.to_csv(path2)
     except:
         pass
 
     # Скачиваем таблицу состояний
     global tree
-    path1 = Path("C:/Users/50AdmNsk/PycharmProjects/pythonProject/tree.xlsx")
+    path1 = os.path.abspath("tree.xlsx")
     tree = pd.read_excel(path1)
     del (tree['Unnamed: 0'])
 
@@ -278,7 +279,7 @@ def callback_query(call):
         #                                     callback_data='type3' + str(i / 2)))  # Создаем соответствующую кнопку
         bot.send_message(call.message.chat.id, emoji.emojize(
             "Идет загрузка. Пожалуйста, подождите!"), reply_markup=markup)
-        bot.send_document(call.message.chat.id, open(r"C:/Users/50AdmNsk/PycharmProjects/pythonProject/Клиники.xlsx", 'rb'))
+        bot.send_document(call.message.chat.id, os.path.abspath("/Клиники.xlsx"))
         markup = InlineKeyboardMarkup()  # Определяем кнопку
         markup.add(InlineKeyboardButton(text=f'Начнем', callback_data=f'start'))
         bot.send_message(call.message.chat.id, emoji.emojize(
